ad4b68eb7681934be3885f1e6ab5670711093d4d
winner siabi <w.siabi@gems-consult.com>
2021-09-25 12:16:10 +0100

    Bind back to frontend

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,40 @@ import React, { Component } from 'react'
 import Navbar from './layouts/Navbar'
 
 export class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {user : {}, userData : []}; //
+        this.handleChange = this.handleChange.bind(this) //our custom method has to be binded 
+        this.handleSubmit = this.handleSubmit.bind(this) //binding our custom method to the constructor
+      }
+
+    componentDidMount(){
+        this.handleSubmit();
+    }
+
+    handleChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name] : value
+        })
+    }
+
+    handleSubmit(){
+        const url = 'http://localhost:5000/user/login'
+        fetch(url,{
+            method : 'POST',
+            headers :{
+                type :'application/json'
+            },
+            body : JSON.stringify(this.state.userData)
+        }).then(response => response.json())
+        .then(data => this.user.setState(data))
+        .catch(err => console.log(err))
+    }
+
     render() {
+        console.log(this.state.user);
         return (
            <React.Fragment>
                <Navbar />
@@ -11,14 +44,14 @@ export class Login extends Component {
                    <h2>Login</h2>
                    </div>
                <div className="login-form" style={{width:"400px",margin:"auto"}} >
-                       <form className="row gy-2 g-3 gx-3 align-items-center">
+                       <form className="row gy-2 g-3 gx-3 align-items-center" method='post' onSubmit={this.handleSubmit} >
                            <div className="form-group">
                                <label>Email</label>
-                               <input type="email" className="form-control" />
+                               <input type="email" className="form-control" name="email" value={this.state.userData.email} onChange={this.handleChange} />
                            </div>
                            <div className="form-group">
                                <label>Password</label>
-                               <input type="password" className="form-control" />
+                               <input type="password" name="password" value={this.state.userData.password} className="form-control" onChange={this.handleChange} />
                            </div>
                            <div className="col-auto">
                                <button type="submit" className="btn btn-primary">Login</button>
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,34 @@ import React, { Component } from 'react'
 import Navbar from './layouts/Navbar'
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { userData : []}; //
+        this.handleChange = this.handleChange.bind(this) //our custom method has to be binded 
+      }
+
+      handleChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        this.userData.setState({
+            [name] : value
+        })
+        //[name] : value
+    }
+
+    componentDidMount(){
+        const url = 'http://localhost:5000/user/signup';
+        fetch(url,{
+            method : 'POST',
+            headers :{
+                type :'application/json'
+            },
+            body : JSON.stringify(this.state.userData)
+        }).then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    }
+
     render() {
         return (
            <React.Fragment>
@@ -14,19 +42,19 @@ class Register extends Component {
                        <form className="row gy-2 g-3 gx-3 align-items-center">
                            <div className="form-group">
                                <label>Fullname</label>
-                               <input className="form-control" />
+                               <input className="form-control" onChange={this.handleChange} name="fullname" />
                            </div>
                            <div className="form-group">
                                <label>Email</label>
-                               <input type="email" className="form-control" />
+                               <input type="email" className="form-control" onChange={this.handleChange} name="email" />
                            </div>
                            <div className="form-group">
                                <label>Password</label>
-                               <input type="password" className="form-control" />
+                               <input type="password" className="form-control" onChange={this.handleChange} name="password" />
                            </div>
                            <div className="form-group">
                                <label>Place of Origin</label>
-                               <input className="form-control" />
+                               <input className="form-control" onChange={this.handleChange} name="place_origin" />
                            </div>
                            <div>
                                 <input class="form-check-input" type="checkbox" id="gridCheck"/>
